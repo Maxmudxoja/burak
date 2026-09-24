ee1757ad02b5c7e39c059a3255583c11bebcd576
Makhmudkhuja <3110MMM3110@email.com>
2026-09-24 14:21:59 +0900

    feat: Train.ts da M task bajarildi

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,16 @@
-console.log("Hello World");
+//MIT tasks
+
+//M-Task
+interface NumberSquare {
+  number: number;
+  square: number;
+}
+
+function getSquareNumbers(numbers: number[]): NumberSquare[] {
+  return numbers.map((num) => ({
+    number: num,
+    square: num * num,
+  }));
+}
+
+console.log(getSquareNumbers([1, 2, 3]));
